5a24363c9bf77af5d3e73cc0efc7743fac822468
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2024-08-19 13:30:39 +0530

    Hero section work completed

--- src/page.tsx
+++ src/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./common/header";
+import Work from "./components/work";
 
 export default function Page() {
     return <>
-        <div className="w-full h-screen text-white bg-black">
+        <div className="w-full font-['satoshi_variable'] h-screen text-white bg-black">
             <Header/>
+            <Work/>
         </div>
     </>;
 }
