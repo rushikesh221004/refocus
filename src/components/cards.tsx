dcd8a70f0a134656c834f81e234e89fdc6455497
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2024-08-23 21:34:36 +0530

    Footer description added completed

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -3,7 +3,7 @@ import Card from "./card";
 export default function Cards() {
     return <>
         <div className="w-full">
-            <div className="flex items-center max-w-screen-xl gap-2 py-20 mx-auto">
+            <div className="flex items-center max-w-screen-xl gap-2 pt-20 mx-auto">
                 <Card width={"basis-1/3"} start={false} para={true} />
                 <Card width={"basis-2/3"} start={true} para={false} />
             </div>
